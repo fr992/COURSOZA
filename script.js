148a700a3ade72c77b0bc0a1c49696bb350897d2
fr992 <155923147+fr992@users.noreply.github.com>
2024-11-25 17:13:36 +0100

    subjectline fr2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function() {
-  var navbar = document.querySelector('.navbar');
+  var navbar = document.querySelector('.priv-navbar');
     if (window.scrollY > 100) {
         navbar.classList.add('fixed');
     } else {
